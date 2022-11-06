e0a1592b571b0c30185238080647148c94482bc5
Sang Nguyen Tan <sos.coconut@gmail.com>
2022-11-06 14:53:34 +0700

    Login and Navigation

---
+++ fms_employee/lib/router.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:fms_employee/screens/login_screen.dart';
+import 'package:fms_employee/screens/staff_home_page.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case LoginScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const LoginScreen(),
+      );
+    case StaffHomePage.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const StaffHomePage(),
+      );
+    default:
+      return MaterialPageRoute(
+          settings: routeSettings,
+          builder: (_) => const Scaffold(
+            body: Center(
+              child: Text('Trang không tồn tại'),
+            ),
+          ));
+  }
+}
