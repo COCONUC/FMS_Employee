e165f01a1f592ea1c8cbc8ae7d9da94a85aeb535
Sang Nguyen Tan <sos.coconut@gmail.com>
2022-11-08 19:27:54 +0700

    Add profile screen and fixing data

--- fms_employee/lib/data/data_file.dart
+++ fms_employee/lib/data/data_file.dart
@@ -4,14 +4,14 @@ import 'package:fms_employee/models/model_booking.dart';
 class DataFile {
 
   static List<ModelBooking> bookingList = [
-    ModelBooking("Cleaning", "Today, 11:00 am", "booking_owner.png",
-        "Mendy Wilson", "Approved", "Today"),
-    ModelBooking("Cleaning", "Today, 08:00 am", "booking_owner1.png",
-        "Jenny Winget", "Approved", "Today"),
-    ModelBooking("Cleaning", "Today, 07:00 am", "booking_owner2.png",
-        "Jacob Jones", "Approved", "Today"),
-    ModelBooking("Cleaning", "Yesterday, 06:00 am", "booking_owner3.png",
-        "Ronald Richards", "Approved", "Yesterday")
+    ModelBooking("Vệ sinh sopha", "Hôm nay, 11:00 am", "booking_owner.png",
+        "Tên khách hàng", "Approved", "Hôm Nay"),
+    ModelBooking("Vệ sinh sopha", "Hôm nay, 08:00 am", "booking_owner1.png",
+        "Tên khách hàng", "Approved", "Hôm Nay"),
+    ModelBooking("Vệ sinh sopha", "Hôm nay, 07:00 am", "booking_owner2.png",
+        "Tên khách hàng", "Approved", "Hôm Nay"),
+    ModelBooking("Vệ sinh sopha", "ngày/tháng/năm, 06:00 am", "booking_owner3.png",
+        "Tên khách hàng", "Approved", "1 Ngày Trước")
   ];
 
 }
